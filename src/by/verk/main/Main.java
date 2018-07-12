0149d72390f5198e09cc209bfadaff681df94f0d
Pavel Verkhovtsov <verchpasha@gmail.com>
2018-07-12 12:47:09 +0300

    Initial commit

---
+++ src/by/verk/main/Main.java
@@ -0,0 +1,22 @@
+package by.verk.main;
+
+import static by.verk.pattermMatching.Matcher.match;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getValue("1"));
+        System.out.println(getValue(1));
+        System.out.println(getValue(1.0));
+        System.out.println(getValue('b'));
+
+    }
+
+    private static Object getValue(Object o) {
+        return match(String.class)
+                .when(String.class).then((s) -> "String value: "+ s)
+                .when(Integer.class).then((i) -> "Integer value: "+ i)
+                .when(Double.class).then((d) -> "Double value: " + d)
+        .apply(o);
+    }
+}
